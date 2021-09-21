66404afb476b65d8c5a20b4d1a0760ec43d8c805
Anthony Hill <hillganthony@outlook.com>
2021-09-21 11:01:35 -0400

    commit tree 27e90501e722ad6dfd979a6d8ddf22a0cedc9cb9

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+
+if (199 <= 400) {
+return 'This one is on me!';
+};
+
+if (2001 > 2000) {
+  return 'I will gladly take your thirty bucks.';
+};
+
+if (2502 > 2500) {
+  return 'No can do.';
+};
+
+
+
+
 }
 
 function ternaryCheckCity(){
